178613efff2b17bc45b7e45e27b3863d986c324d
andrewburch <andrewwburch@gmail.com>
2016-10-12 06:30:03 -0700

    Removed preloader and organized code.

--- NetBeans/CertificateCreator/src/certificatecreator/CertificateCreator.java
+++ NetBeans/CertificateCreator/src/certificatecreator/CertificateCreator.java
@@ -30,7 +30,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -40,9 +39,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import static javafx.scene.paint.Color.rgb;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import static javafx.scene.paint.Color.rgb;
 
 /**
  *
@@ -66,9 +65,13 @@ public class CertificateCreator extends Application {
         StringProperty stdntLstTxt = new SimpleStringProperty();
         List<String> studentList = new ArrayList<>();
         Label statusText = createStatusText(stdntLstTxt);
+        Label fieldInstructions = new Label("Type name of desired student in " +
+                "textbox. The student list will filter as you type.");
+        Label listInstructions = new Label("Click name of desired student in " +
+                "list to create certificate.");
         System.out.println("Directory path:\n" + stdntLstPath);
         if (stdntLstPath.isEmpty()) {
-            stdntLstTxt.set("No student list has been selected");
+            stdntLstTxt.set("No student list has been selected!");
             statusText.setTextFill(rgb(255, 0, 0));
         }
         else {
@@ -81,7 +84,8 @@ public class CertificateCreator extends Application {
                 stdntFld);
         Button setDirBtn = createDirectoryButton(configPath, primaryStage);
         Scene scene = new Scene(createLayout(stdntLbl, stdntFld,
-                visibleStudentList, setDirBtn, statusText));
+                visibleStudentList, setDirBtn, statusText, fieldInstructions,
+                listInstructions));
         scene.getStylesheets().add(CertificateCreator.class.getResource(
                 "Main.css").toExternalForm());
         primaryStage.setTitle("Certificate Writer");
@@ -93,14 +97,7 @@ public class CertificateCreator extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
-    private String getDate() {
-        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
-        Calendar cal = Calendar.getInstance();
-        String date = dateFormat.format(cal.getTime());
-        return date;
-    }
-    
+
     private ListView createVisibleStudentList(List<String> studentList,
             TextField stdntFld) {
         ObservableList boxList = FXCollections.observableList(studentList);
@@ -114,23 +111,17 @@ public class CertificateCreator extends Application {
             Platform.runLater(() -> {
                 if (selected == null || !selected.equals(stdntFld.getText())) {
                     filteredItems.setPredicate(boxListItem -> {
-                        if(boxListItem.toUpperCase().startsWith(newVal.
-                                toUpperCase())) {
-                            return true;
-                        } else {
-                            return false;
-                        }
+                        return boxListItem.toUpperCase().startsWith(newVal.
+                                toUpperCase());
                     });
                 }
             });
         });
         visibleStudentList.setItems(filteredItems);
-        visibleStudentList.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                selectedStudent = visibleStudentList.getSelectionModel()
-                        .getSelectedItem();
-            }
+        visibleStudentList.setOnMouseClicked((MouseEvent event) -> {
+            selectedStudent = visibleStudentList.getSelectionModel()
+                    .getSelectedItem();
+            System.out.println("Selected stduent:\n" + selectedStudent);
         });
         return visibleStudentList;
     }
@@ -141,6 +132,111 @@ public class CertificateCreator extends Application {
         statusText.textProperty().bind(stdntLstTxt);
         //statusText.setFill(rgb(0, 0, 255));
         return statusText;
+    }
+    
+    private Button createDirectoryButton(String filPath, Stage primaryStage) {
+        FileChooser chooseList = new FileChooser();
+        chooseList.setTitle("Select Student File");
+        Button dirBtn = new Button("Set Student List");
+        dirBtn.setId("dirBtn");
+        dirBtn.setOnAction((final ActionEvent a) -> {
+            File file = chooseList.showOpenDialog(primaryStage);
+            if (file!=null) {
+                createConfig(filPath, file.toString());
+                restartApplication();
+            }
+        });
+        return dirBtn;
+    }
+    
+    private HBox createLayout(Label stdntLbl, TextField stdntFld,
+            ListView<String> stdntListView, Button setDirBtn, Label statusText,
+            Label fieldInstructions, Label listInstructions) {
+        VBox studentInfoDiv1 = new VBox();
+            studentInfoDiv1.getChildren().add(stdntLbl);
+            studentInfoDiv1.getChildren().add(stdntFld);
+        VBox studentInfoDiv2 = new VBox();
+            studentInfoDiv2.setId("studentInfoDiv2");
+            studentInfoDiv2.getChildren().add(studentInfoDiv1);
+            studentInfoDiv2.getChildren().add(setDirBtn);
+            studentInfoDiv2.getChildren().add(statusText);
+            statusText.setTextOverrun(LEADING_ELLIPSIS);
+        VBox studentInfoDiv3 = new VBox();
+            studentInfoDiv3.setId("studentInfoDiv3");
+            studentInfoDiv3.getChildren().add(fieldInstructions);
+            fieldInstructions.setWrapText(true);
+            studentInfoDiv3.getChildren().add(listInstructions);
+            listInstructions.setWrapText(true);
+        VBox studentInfoDiv4 = new VBox();
+            studentInfoDiv4.setId("studentInfoDiv4");
+            studentInfoDiv4.getChildren().add(studentInfoDiv2);
+            studentInfoDiv4.getChildren().add(studentInfoDiv3);
+        HBox layout = new HBox();
+            layout.setId("layout");
+            layout.getChildren().add(studentInfoDiv4);
+            layout.getChildren().add(stdntListView);
+        return layout;
+    }
+    
+    private void createConfig(String filPath, String text) {
+        try (Writer w = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(new File(filPath))))){
+            w.write(text);
+            w.close();
+            } catch (IOException io) {
+                    System.err.println(" IOException: " + io);
+            }
+    }
+    
+    public void restartApplication() {
+        final String javaBin = System.getProperty("java.home")
+                + File.separator + "bin" + File.separator + "java";
+        try {
+            final File currentJar = new File(CertificateCreator.class
+                    .getProtectionDomain().getCodeSource().getLocation()
+                    .toURI());
+            if(!currentJar.getName().endsWith(".jar"))
+            return;
+            final ArrayList<String> command = new ArrayList<>();
+            command.add(javaBin);
+            command.add("-jar");
+            command.add(currentJar.getPath());
+            final ProcessBuilder builder = new ProcessBuilder(command);
+            try{
+                builder.start();
+            } catch (IOException io) {
+                System.out.println("I/O exception in restartApplication, "
+                        + "program could not be restarted." + io);
+            }
+            System.exit(0);
+            } catch (URISyntaxException uri) {
+                System.out.println("URI sytanx exception in restartApplication: "
+                        + uri);
+        }
+    }
+        
+    private String getDate() {
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
+        Calendar cal = Calendar.getInstance();
+        String date = dateFormat.format(cal.getTime());
+        return date;
+    }
+        
+    private String getStudentList(String filPath) {
+        Path filePath = Paths.get(filPath);
+        System.out.println("file path:\n" + filePath);
+        String stdntLstPath = "";
+        try{
+            List<String> filPathLines = Files.readAllLines(filePath);
+            if(filPathLines.isEmpty()) {   
+            } else {
+                stdntLstPath = filPathLines.get(0);
+            }
+            
+        } catch (IOException io) {
+            createConfig(filPath, "");
+        }
+        return stdntLstPath;
     }
     
     private void extractStudentList(List<String> studentList,
@@ -172,90 +268,5 @@ public class CertificateCreator extends Application {
                     + io);
         }
     }
-    
-    private String getStudentList(String filPath) {
-        Path filePath = Paths.get(filPath);
-        System.out.println("file path:\n" + filePath);
-        String stdntLstPath = "";
-        try{
-            List<String> filPathLines = Files.readAllLines(filePath);
-            if(filPathLines.isEmpty()) {   
-            } else {
-                stdntLstPath = filPathLines.get(0);
-            }
-            
-        } catch (IOException io) {
-            createConfig(filPath, "");
-        }
-        return stdntLstPath;
-    }
-    
-    private HBox createLayout(Label stdntLbl, TextField stdntFld,
-            ListView<String> stdntListView, Button setDirBtn, Label statusText) {
-        VBox studentName = new VBox();
-        studentName.getChildren().add(stdntLbl);
-        studentName.getChildren().add(stdntFld);
-        VBox studentInfo = new VBox();
-        studentInfo.setId("studentInfo");
-        studentInfo.getChildren().add(studentName);
-        studentInfo.getChildren().add(setDirBtn);
-        studentInfo.getChildren().add(statusText);
-        statusText.setTextOverrun(LEADING_ELLIPSIS);
-        HBox layout = new HBox();
-        layout.setId("layout");
-        layout.getChildren().add(studentInfo);
-        layout.getChildren().add(stdntListView);
-        return layout;
-    }
-    
-    private void createConfig(String filPath, String text) {
-        try (Writer w = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(new File(filPath))))){
-            w.write(text);
-            w.close();
-            } catch (IOException io) {
-                    System.err.println(" IOException: " + io);
-            }
-    }
-    
-    private Button createDirectoryButton(String filPath, Stage primaryStage) {
-        FileChooser chooseList = new FileChooser();
-        chooseList.setTitle("Select Student File");
-        Button dirBtn = new Button("Set Student List");
-        dirBtn.setId("dirBtn");
-        dirBtn.setOnAction((final ActionEvent a) -> {
-            File file = chooseList.showOpenDialog(primaryStage);
-            if (file!=null) {
-                createConfig(filPath, file.toString());
-                restartApplication();
-            }
-        });
-        return dirBtn;
-    }
-    
-    public void restartApplication() {
-        final String javaBin = System.getProperty("java.home")
-                + File.separator + "bin" + File.separator + "java";
-        try {
-            final File currentJar = new File(CertificateCreator.class
-                    .getProtectionDomain().getCodeSource().getLocation()
-                    .toURI());
-            if(!currentJar.getName().endsWith(".jar"))
-            return;
-            final ArrayList<String> command = new ArrayList<>();
-            command.add(javaBin);
-            command.add("-jar");
-            command.add(currentJar.getPath());
-            final ProcessBuilder builder = new ProcessBuilder(command);
-            try{
-                builder.start();
-            } catch (IOException io) {
-                System.out.println("I/O exception, program could not be "
-                        + "restarted." + io);
-            }
-            System.exit(0);
-            } catch (URISyntaxException uri) {
-                System.out.println("URI sytanx exception: " + uri);
-        }
-    }
+
 }
